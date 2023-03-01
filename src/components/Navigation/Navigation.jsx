cfd4f1cdfd786b1c65379ab0b457dbfdfc79c11d
Anna Volfman <aniya_07@ukr.net>
2023-03-01 23:38:33 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { selectToken } from 'redux/auth/authSelectors';
 
 export function Navigation() {
+
+  const isLoggedIn = useSelector(selectToken);
+
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </nav>
   );
 }
